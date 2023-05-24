c1740dff9af3a74ee133e327099a8064c30b2cac
José Simão <simaojm@gmail.com>
2023-05-24 01:36:25 -0300

    first commit

---
+++ src/main/java/br/com/ifsp/Pessoas/Pessoa.java
@@ -0,0 +1,61 @@
+package br.com.ifsp.Pessoas;
+
+import java.util.UUID;
+
+public class Pessoa {
+	private final int cod;
+	private String nome;
+	private String sobrenome;
+	private int idade;
+	private String profissao;
+	private final String uuid;
+	
+	// Constructor
+	public Pessoa(int cod, String nome, String sobrenome, int idade, String profissao) {
+		super();
+		this.cod = cod;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.idade = idade;
+		this.profissao = profissao;
+		this.uuid = UUID.randomUUID().toString();
+	}
+
+	// Getters and setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+		
+}
